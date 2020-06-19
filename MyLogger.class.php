b396faf486b1a14058a59538edf3974126e748f8
RubenvEijkeren <54809240+RubenvEijkeren@users.noreply.github.com>
2020-06-19 12:15:10 +0200

    Uitwerking van de exercise

--- MyLogger.class.php
+++ MyLogger.class.php
@@ -7,7 +7,7 @@ class MyLogger
     
     public function log($message, $level)
     {
-        echo $level . ": " . $message;
+        $this->logWithTime($level . ": " . $message);
     }
     public function warning($message)
     {
@@ -25,9 +25,12 @@ class MyLogger
     {
         $this->log($message, "DEBUG");
     }
+    private function logWithTime($message)
+    {
+        date_default_timezone_set('Europe/Amsterdam');
+        echo Date("Y-m-d H:i:s") . " " . $message;
+    }
 }
 $logger = new MyLogger();
 $logger->error("dit is een error");
-$logger->log("Hello world!", "INFO");
-$logger->info("this is an info log");
 ?>
